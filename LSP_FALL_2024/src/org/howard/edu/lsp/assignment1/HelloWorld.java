4728b774058615dfb8af198db698a9b37d6e510e
gaurabh <gaurabh@localhost>
2024-08-21 18:11:05 -0400

    First commit of assignment 1.

---
+++ LSP_FALL_2024/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.howard.edu.lsp.assignment1;
+
+/**
+ * Name: Gaurav Adhikari
+ */
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+}
